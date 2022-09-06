fc023933d0fb594c52dee5a6fc1d2e7cf5af17dc
edward <2219080450@qq.com>
2022-09-07 00:30:31 +0800

    自己封装 Chart 组件 研究了很久

---
+++ src/components/Chart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>$END$</div>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component
+    export default class Chart extends Vue {
+
+    }
+</script>
+
+<style scoped lang="scss">
+
+</style>
