4d671a2156c9f78267c01c2746e32e9eaf6c252c
edward <2219080450@qq.com>
2022-09-07 22:27:03 +0800

    完善 Chart 细节样式

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,17 +1,25 @@
 <template>
-    <div>$END$</div>
+    <div class="wrapper" ref="wrapper">chart</div>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-    import {Component} from 'vue-property-decorator';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import * as echarts from 'echarts';
 
     @Component
     export default class Chart extends Vue {
+        @Prop() options: any;
 
+        mounted() {
+            console.log(this.options);
+            const chart = echarts.init(this.$refs.wrapper as HTMLDivElement)
+            chart.setOption(this.options)
+        }
     }
 </script>
 
 <style scoped lang="scss">
-
+    .wrapper{
+        height: 400px;
+    }
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,13 +1,18 @@
 <template>
     <Layout>
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+        <div class="chart-wrapper" ref="chartWrapper">
+            <Chart class="chart" :options="x"/>
+        </div>
         <ol v-if="groupedList.length>0">
             <li v-for="(group,index) in groupedList" :key="index">
-                <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span>></h3>
+                <h3 class="title">
+                    {{beautify(group.title)}}
+                    <span>￥{{group.total}}</span>
+                </h3>
                 <ol>
                     <li v-for="item in group.items" :key="item.id"
-                        class="record"
-                    >
+                        class="record">
                         <span>{{tagString(item.tags)}}</span>
                         <span class="notes">{{item.notes}}</span>
                         <span>￥{{item.amount}} </span>
@@ -16,7 +21,7 @@
             </li>
         </ol>
         <div v-else class="noResult">
-
+            目前没有相关记录
         </div>
 
     </Layout>
@@ -30,13 +35,19 @@
     import recordTypeList from '@/constants/recordTypeList';
     import dayjs from 'dayjs';
     import clone from '@/lib/clone';
+    import Chart from '@/components/Chart.vue';
 
     @Component({
-        components: {Tabs},
+        components: {Chart, Tabs},
     })
     export default class Statistics extends Vue {
         tagString(tags: Tag[]) {
-            return tags.length === 0 ? '无' : tags.map(t=>t.name).join('，');
+            return tags.length === 0 ? '无' : tags.map(t => t.name).join('，');
+        }
+
+        mounted() {             //实现默认图表生成最后的几组数据
+            const div = (this.$refs.chartWrapper as HTMLDivElement);
+            div.scrollLeft = div.scrollWidth;
         }
 
         beautify(string: string) {
@@ -55,9 +66,52 @@
             }
         }
 
+        get x() {
+            return {
+                grid: {          //控制echarts图像的边距
+                    left: 0,
+                    right: 0,
+                },
+                xAxis: {
+                    type: 'category',
+                    data: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                        '11', '12', '13', '14', '15', '16', '17',
+                        '18', '19', '20', '21', '22', '23', '24',
+                        '25', '26', '27', '28', '29', '30'
+                    ],
+                    axisTick: {alignWithLabel: true},  //设置横轴刻度线与数据展示对齐
+                    axisLine: {lineStyle: {color: '#666'}}
+                },
+                yAxis: {
+                    type: 'value',
+                    show: false,
+                },
+                series: [
+                    {
+                        symbol:'circle',     //设置标签点的样式
+                        symbolSize: 10,     //设置标签点的大小
+                        itemStyle: {color: '#666'},
+                        data: [
+                            150, 230, 224, 218, 135, 147, 260,
+                            150, 230, 224, 218, 135, 147, 260,
+                            150, 230, 224, 218, 135, 147, 260,
+                            150, 230, 224, 218, 135, 147, 260, 1, 2
+                        ],
+                        type: 'line'
+                    }
+                ],
+                tooltip: {
+                    show: true,
+                    position:'top',
+                    formatter:'{c}'    //提示框下缘有个尖
+                }
+            };
+        }
+
         get recordList() {
             return (this.$store.state as RootState).recordList;
         }
+
         get groupedList() {
             const {recordList} = this;
             const newList = clone(recordList)
@@ -67,7 +121,7 @@
                 return [];
             }
             type Result = { title: string, total?: number, items: RecordItem[] }[]
-            const result:Result = [{title: dayjs(recordList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
+            const result: Result = [{title: dayjs(recordList[0].createdAt).format('YYYY-MM-DD'), items: [newList[0]]}];
             for (let i = 1; i < newList.length; i++) {
                 const current = newList[i];
                 const last = result[result.length - 1];
@@ -94,11 +148,12 @@
     };
 </script>
 <style scoped lang="scss">
-    .noResult{
+    .noResult {
         padding: 16px;
         text-align: center;
 
     }
+
     ::v-deep {
         .type-tabs-item {
             background: #c4c4c4;
@@ -138,5 +193,16 @@
         margin-right: auto;
         margin-left: 16px;
         color: #999;
+    }
+
+    .chart {
+        width: 430%;
+
+        &-wrapper {
+            overflow: auto; //图表滚动条
+            &::-webkit-scrollbar { //隐藏滚动条，pc端按住shift用鼠标滚轮查看图像
+                display: none;
+            }
+        }
     }
 </style>
