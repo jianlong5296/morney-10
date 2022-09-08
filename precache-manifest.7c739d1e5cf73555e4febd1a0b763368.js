8aee1fafec45223e4207a80c12ace4c30f0abced
edward <2219080450@qq.com>
2022-09-08 21:37:02 +0800

    deploy

---
+++ precache-manifest.7c739d1e5cf73555e4febd1a0b763368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d7c2ba991cc84073e86",
+    "url": "/morney-10/css/app.ade57f06.css"
+  },
+  {
+    "revision": "fa2716f8222b5a0f830f9843d0d54292",
+    "url": "/morney-10/index.html"
+  },
+  {
+    "revision": "2d7c2ba991cc84073e86",
+    "url": "/morney-10/js/app.abbe5ce5.js"
+  },
+  {
+    "revision": "71a6b04010c5b4de42a6",
+    "url": "/morney-10/js/chunk-vendors.1e1910f3.js"
+  },
+  {
+    "revision": "b0c72820654d086f2b526db8c6a20387",
+    "url": "/morney-10/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-10/robots.txt"
+  }
+]);
